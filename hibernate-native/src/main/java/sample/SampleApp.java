52c3277e04e4e25879683a4314faba8f8ec06324
Vedran Pavic <vedran.pavic@gmail.com>
2016-08-26 07:17:33 +0200

    Extract initializer

--- hibernate-native/src/main/java/sample/SampleApp.java
+++ hibernate-native/src/main/java/sample/SampleApp.java
@@ -1,30 +1,13 @@
 package sample;
 
-import org.hibernate.SessionFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootApplication
-public class SampleApp implements CommandLineRunner {
-
-	private SessionFactory sessionFactory;
-
-	public SampleApp(SessionFactory sessionFactory) {
-		this.sessionFactory = sessionFactory;
-	}
+public class SampleApp {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SampleApp.class, args);
-	}
-
-	@Override
-	@Transactional
-	public void run(String... args) throws Exception {
-		this.sessionFactory.getCurrentSession().save(new SampleEntity("sample1"));
-		this.sessionFactory.getCurrentSession().save(new SampleEntity("sample2"));
-		this.sessionFactory.getCurrentSession().save(new SampleEntity("sample3"));
 	}
 
 }
