ec8cc7c05ce432e9d8502e849cf728eb806b1f73
Vedran Pavic <vedran.pavic@gmail.com>
2017-01-27 00:20:26 +0100

    Add session-jdbc sample

---
+++ session-jdbc/src/main/java/sample/SecurityConfig.java
@@ -0,0 +1,24 @@
+package sample;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// @formatter:off
+		http
+			.requestMatchers()
+				.antMatchers("/", "/login")
+				.and()
+			.formLogin()
+				.and()
+			.authorizeRequests()
+				.anyRequest().authenticated();
+		// @formatter:on
+	}
+
+}
