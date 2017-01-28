5476374b817bc126bff4cced9d2737ad69718a38
Vedran Pavic <vedran.pavic@gmail.com>
2017-01-28 23:13:12 +0100

    Simplify session-jdbc security config

--- session-jdbc/src/main/java/sample/SecurityConfig.java
+++ session-jdbc/src/main/java/sample/SecurityConfig.java
@@ -1,24 +1,11 @@
 package sample;
 
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
+@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		// @formatter:off
-		http
-			.requestMatchers()
-				.antMatchers("/", "/login")
-				.and()
-			.formLogin()
-				.and()
-			.authorizeRequests()
-				.anyRequest().authenticated();
-		// @formatter:on
-	}
-
 }
