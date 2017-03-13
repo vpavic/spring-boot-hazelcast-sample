e02456a989fd5d9d94f126a723b1cdab8ec8ac48
Vedran Pavic <vedran.pavic@gmail.com>
2017-03-13 22:51:31 +0100

    Harmonize test names

--- session-hazelcast/src/test/java/sample/SessionHazelcastSampleAppTests.java
+++ session-hazelcast/src/test/java/sample/SessionHazelcastSampleAppTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SampleAppTests {
+public class SessionHazelcastSampleAppTests {
 
 	@Test
 	public void contextLoads() {
