2e3461a65b98235a378c6470d73dc94d8058c6ee
Vedran Pavic <vedran.pavic@gmail.com>
2017-03-15 20:55:10 +0100

    Use TestContainers for tests

--- session-mongo/src/test/java/sample/SessionMongoSampleAppTests.java
+++ session-mongo/src/test/java/sample/SessionMongoSampleAppTests.java
@@ -1,19 +1,31 @@
 package sample;
 
+import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.testcontainers.containers.GenericContainer;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.util.EnvironmentTestUtils;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.session.data.mongo.AbstractMongoSessionConverter;
 import org.springframework.session.data.mongo.MongoOperationsSessionRepository;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = SessionMongoSampleApp.class)
+@ContextConfiguration(initializers = SessionMongoSampleAppTests.Initializer.class)
 public class SessionMongoSampleAppTests {
+
+	@ClassRule
+	public static GenericContainer mongo = new GenericContainer("mongo:3.4.2")
+			.withExposedPorts(27017);
 
 	@Autowired
 	private MongoOperationsSessionRepository sessionRepository;
@@ -29,6 +41,19 @@ public class SessionMongoSampleAppTests {
 	public void sessionRepositoryUsesConfiguredSessionMongoConverter() {
 		assertThat(ReflectionTestUtils.getField(this.sessionRepository, "mongoSessionConverter"))
 				.isEqualTo(this.mongoSessionConverter);
+	}
+
+	public static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+		@Override
+		public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+			String host = mongo.getContainerIpAddress();
+			Integer port = mongo.getMappedPort(27017);
+			EnvironmentTestUtils.addEnvironment("testcontainers",
+					configurableApplicationContext.getEnvironment(),
+					"spring.data.mongodb.uri=mongodb://" + host + ":" + port + "/test");
+		}
+
 	}
 
 }
--- session-websocket/src/test/java/sample/SessionWebSocketSampleAppTests.java
+++ session-websocket/src/test/java/sample/SessionWebSocketSampleAppTests.java
@@ -1,17 +1,39 @@
 package sample;
 
+import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.testcontainers.containers.GenericContainer;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.util.EnvironmentTestUtils;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SessionWebSocketSampleAppTests {
 
+	@ClassRule
+	public static GenericContainer redis = new GenericContainer("redis:3.0.7")
+			.withExposedPorts(6379);
+
 	@Test
 	public void contextLoads() {
+	}
+
+	public static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+		@Override
+		public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+			String host = redis.getContainerIpAddress();
+			Integer port = redis.getMappedPort(6379);
+			EnvironmentTestUtils.addEnvironment("testcontainers",
+					configurableApplicationContext.getEnvironment(),
+					"spring.redis.host=" + host, "spring.redis.port=" + port);
+		}
+
 	}
 
 }
