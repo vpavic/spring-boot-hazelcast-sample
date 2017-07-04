b2cb2b44e7330a3211d60783931aa189db922795
Vedran Pavic <vedran.pavic@gmail.com>
2017-07-04 23:29:36 +0200

    Add hazelcast-use-cases sample

---
+++ hazelcast-use-cases/src/main/java/demo/HazelcastUseCasesApplication.java
@@ -0,0 +1,36 @@
+package demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HazelcastUseCasesApplication implements CommandLineRunner {
+
+	@Autowired
+	private SampleEntityRepository repository;
+
+	public static void main(String[] args) {
+		SpringApplication.run(HazelcastUseCasesApplication.class, args);
+	}
+
+	@Override
+	@Transactional
+	public void run(String... args) throws Exception {
+		this.repository.save(new SampleEntity("sample1"));
+		this.repository.save(new SampleEntity("sample2"));
+		this.repository.save(new SampleEntity("sample3"));
+	}
+
+	@GetMapping(path = "/")
+	public Iterable<SampleEntity> home() {
+		return this.repository.findAll();
+	}
+
+
+}
---
+++ hazelcast-use-cases/src/main/java/demo/SampleEntity.java
@@ -0,0 +1,36 @@
+package demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+@Entity
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+public class SampleEntity {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	private String name;
+
+	public SampleEntity(String name) {
+		this.name = name;
+	}
+
+	public Long getId() {
+		return this.id;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	private SampleEntity() {
+		// for Hibernate
+	}
+
+}
