4a95aa58d3d7b9ae87e93adc092f6ed4625c68b1
Vedran Pavic <vedran.pavic@gmail.com>
2017-09-29 23:58:56 +0200

    Upgrade Spring Boot to 2.0.0.M4

--- hazelcast-use-cases/src/main/java/demo/SecurityConfiguration.java
+++ hazelcast-use-cases/src/main/java/demo/SecurityConfiguration.java
@@ -1,11 +1,36 @@
 package demo;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import java.util.Collections;
+
+import org.springframework.boot.autoconfigure.security.StaticResourceRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
-@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
-class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+		return new InMemoryUserDetailsManager(Collections.singletonList(
+				User.withUsername("user").password("password").roles("USER").build()));
+	}
+
+	// @formatter:off
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+				.requestMatchers(StaticResourceRequest.toCommonLocations()).permitAll()
+				.anyRequest().authenticated()
+				.and()
+			.formLogin()
+				.permitAll();
+	}
+	// @formatter:on
+
 }
