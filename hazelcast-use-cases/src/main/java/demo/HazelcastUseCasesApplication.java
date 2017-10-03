0461a82bfa60117301d8ab14b3c547586bafad17
Vedran Pavic <vedran.pavic@gmail.com>
2017-10-03 07:32:17 +0200

    Simplify hazelcast-use-cases config

--- hazelcast-use-cases/src/main/java/demo/HazelcastUseCasesApplication.java
+++ hazelcast-use-cases/src/main/java/demo/HazelcastUseCasesApplication.java
@@ -1,9 +1,19 @@
 package demo;
 
+import java.util.Collections;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.StaticResourceRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,5 +42,28 @@ public class HazelcastUseCasesApplication implements CommandLineRunner {
 		return this.repository.findAll();
 	}
 
+	@Configuration
+	static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+		@Bean
+		public UserDetailsService userDetailsService() {
+			return new InMemoryUserDetailsManager(Collections.singletonList(
+					User.withUsername("user").password("password").roles("USER").build()));
+		}
+
+		// @formatter:off
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http
+				.authorizeRequests()
+					.requestMatchers(StaticResourceRequest.toCommonLocations()).permitAll()
+					.anyRequest().authenticated()
+					.and()
+				.formLogin()
+					.permitAll();
+		}
+		// @formatter:on
+
+	}
 
 }
