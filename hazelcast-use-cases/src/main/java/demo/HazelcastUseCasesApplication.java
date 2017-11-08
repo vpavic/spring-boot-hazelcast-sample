ab27b87702d821cf3f2f6b993c032e5020908fa5
Vedran Pavic <vedran.pavic@gmail.com>
2017-11-08 21:46:31 +0100

    Improve hazelcast-use-cases sample

--- hazelcast-use-cases/src/main/java/demo/HazelcastUseCasesApplication.java
+++ hazelcast-use-cases/src/main/java/demo/HazelcastUseCasesApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.StaticResourceRequest;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableCaching
 @RestController
 public class HazelcastUseCasesApplication implements CommandLineRunner {
 
