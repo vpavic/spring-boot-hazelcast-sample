13bbd643461562d958aa19e9bbb8268889a78ce1
Vedran Pavic <vedran.pavic@gmail.com>
2018-04-18 10:52:59 +0200

    Fix hazelcast-use-cases security config

--- hazelcast-use-cases/src/main/java/demo/HazelcastUseCasesApplication.java
+++ hazelcast-use-cases/src/main/java/demo/HazelcastUseCasesApplication.java
@@ -4,11 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,24 +32,6 @@ public class HazelcastUseCasesApplication implements CommandLineRunner {
 	@GetMapping(path = "/")
 	public Iterable<SampleEntity> home() {
 		return this.repository.findAll();
-	}
-
-	@Configuration
-	static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-		@Override
-		protected void configure(HttpSecurity http) throws Exception {
-			// @formatter:off
-			http
-				.authorizeRequests()
-					.requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-					.anyRequest().authenticated()
-					.and()
-				.formLogin()
-					.permitAll();
-			// @formatter:on
-		}
-
 	}
 
 }
