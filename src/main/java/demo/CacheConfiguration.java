b4794ecf0fa0f47f8f781a69f057998b7ede6273
Vedran Pavic <vedran.pavic@gmail.com>
2020-06-26 13:09:15 +0200

    Reduce explicit configuration to minimum

--- src/main/java/demo/CacheConfiguration.java
+++ src/main/java/demo/CacheConfiguration.java
@@ -1,19 +1,10 @@
 package demo;
 
-import org.hibernate.cache.jcache.ConfigSettings;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernatePropertiesCustomizer;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.jcache.JCacheCacheManager;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableCaching
 public class CacheConfiguration {
-
-	@Bean
-	public HibernatePropertiesCustomizer hibernateSecondLevelCacheCustomizer(JCacheCacheManager cacheManager) {
-		return properties -> properties.put(ConfigSettings.CACHE_MANAGER, cacheManager.getCacheManager());
-	}
 
 }
