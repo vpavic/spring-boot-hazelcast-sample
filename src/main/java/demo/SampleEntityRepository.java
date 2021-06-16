2ff11618ed323e087da436b546e075e78b336dee
Vedran Pavic <vedran.pavic@gmail.com>
2021-06-16 07:23:55 +0200

    Cache the results of SampleEntityRepository#findAll

--- src/main/java/demo/SampleEntityRepository.java
+++ src/main/java/demo/SampleEntityRepository.java
@@ -1,7 +1,14 @@
 package demo;
 
+import java.util.List;
+import javax.persistence.QueryHint;
+
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.repository.CrudRepository;
 
 public interface SampleEntityRepository extends CrudRepository<SampleEntity, Long> {
+
+	@QueryHints(@QueryHint(name = org.hibernate.annotations.QueryHints.CACHEABLE, value = "true"))
+	List<SampleEntity> findAll();
 
 }
