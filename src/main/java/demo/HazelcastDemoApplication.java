aa98264d5f2bf6cae2676503f8be4173977d909f
Vedran Pavic <vedran.pavic@gmail.com>
2021-06-16 07:24:58 +0200

    Return 404 if requested entity is not found

--- src/main/java/demo/HazelcastDemoApplication.java
+++ src/main/java/demo/HazelcastDemoApplication.java
@@ -4,10 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @SpringBootApplication
 @RestController
@@ -35,7 +37,7 @@ public class HazelcastDemoApplication implements CommandLineRunner {
 
 	@GetMapping(path = "/{id}")
 	SampleEntity entity(@PathVariable Long id) {
-		return this.repository.findById(id).orElse(null);
+		return this.repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 	}
 
 }
